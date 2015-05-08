23c8ff52a75d6c7667c80a5d7b4b82d900387dd5
greghelton-ios <greg.helton.tx@gmail.com>
2015-05-08 13:49:19 -0500

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Gregory Helton on 5/8/15.
+//  Copyright (c) 2015 Gregory Helton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Alcolator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alcolator
+//
+//  Created by Gregory Helton on 5/8/15.
+//  Copyright (c) 2015 Gregory Helton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
